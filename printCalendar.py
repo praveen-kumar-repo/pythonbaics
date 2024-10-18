b07c31ddd2b3042391287fba9b7356f165d7fb11
Praveen_K <Praveen_K@singaporeair.com.sg>
2024-10-19 00:52:53 +0800

    Move code to git

---
+++ printCalendar.py
@@ -0,0 +1,6 @@
+import calendar
+
+year=2098
+month=10
+
+print(calendar.month(year,month))
---
+++ square.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    print('Enter the number')
+    n = int(input())
+    
+    for i in range(0,n):
+        print(i**2)
+        i+=1 
