2c552f0bcbbc5d2bb55057b032b021453ea17c6b
Praveen_K <Praveen_K@singaporeair.com.sg>
2024-10-19 00:39:15 +0800

    Push the division code to git

---
+++ division.py
@@ -0,0 +1,14 @@
+#Task
+#The provided code stub reads two integers,  and , from STDIN.
+#Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+
+#No rounding or formatting is necessary.
+
+print('Division Task')
+a=int(input('Enter a number for a :'))
+b=int(input('Enter a number for b :'))
+
+c=a//b
+print(c)
+d=a/b
+print(d)
